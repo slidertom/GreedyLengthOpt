7c74e5f975110dd0fd6c59368d24d265c6c756c9
Tomas <slidertom@gmail.com>
2017-10-01 03:41:35 +0300

    cmake - mingw support

--- GreedyLengthOpt/ProjectOptions.cmake
+++ GreedyLengthOpt/ProjectOptions.cmake
@@ -1,3 +1,5 @@
+include(PrecompiledHeader.cmake)
+
 set(CMAKE_CONFIGURATION_TYPES UnicodeDebug UnicodeRelease)
 set(CMAKE_SUPPRESS_REGENERATION TRUE)
 set(CMAKE_CXX_FLAGS_UNICODEDEBUG "")
@@ -36,23 +38,6 @@ function(set_project_dll_mode LibDirPath)
 	set_precompiled_header()
 endfunction(set_project_dll_mode)
 
-function(set_project_dll_mode_nobrowser LibDirPath)
-	file(GLOB_RECURSE sources "*.cpp" "*.h" "*.c" "*.rc" "*.bmp" "*.cur" "*ico")
-	set(root ${CMAKE_CURRENT_SOURCE_DIR})
-	source_group(TREE ${root} FILES ${sources})
-	list(REMOVE_ITEM sources "${CMAKE_CURRENT_SOURCE_DIR}/CMakeFiles/${CMAKE_VERSION}/CompilerIdCXX/CMakeCXXCompilerId.cpp")
-	list(REMOVE_ITEM sources "${CMAKE_CURRENT_SOURCE_DIR}/CMakeFiles/${CMAKE_VERSION}/CompilerIdC/CMakeCCompilerId.c")
-	source_group("" FILES ${sources})
-	add_library	(${PROJECT_NAME} SHARED ${sources})
-	include_root_dir()
-	set_project_bin_lib_properties("${LibDirPath}")
-	set_project_compile_options_nobrowserinfo()
-	set_project_definitions()
-	set_project_compile_flags()
-	set_project_processor_definitions()
-	set_precompiled_header()
-endfunction(set_project_dll_mode_nobrowser)
-
 function(set_project_lib_mode)
 	file(GLOB_RECURSE sources "*.cpp" "*.h" "*.c" "*.rc" "*.bmp" "*.cur" "*ico")
 	set(root ${CMAKE_CURRENT_SOURCE_DIR})
@@ -83,27 +68,12 @@ function(set_project_exe_mode)
 	set_precompiled_header()
 endfunction(set_project_exe_mode)
 
-function(set_project_exe_mode_nobrowser)
-	file(GLOB_RECURSE sources "*.cpp" "*.h" "*.c" "*.rc" "*.bmp" "*.cur" "*ico")
-	set(root ${CMAKE_CURRENT_SOURCE_DIR})
-	source_group(TREE ${root} FILES ${sources})
-	list(REMOVE_ITEM sources "${CMAKE_CURRENT_SOURCE_DIR}/CMakeFiles/${CMAKE_VERSION}/CompilerIdCXX/CMakeCXXCompilerId.cpp")
-	list(REMOVE_ITEM sources "${CMAKE_CURRENT_SOURCE_DIR}/CMakeFiles/${CMAKE_VERSION}/CompilerIdC/CMakeCCompilerId.c")
-	source_group("" FILES ${sources})
-	add_executable(${PROJECT_NAME} ${sources})
-	include_root_dir()
-	set_project_compile_options_nobrowserinfo()
-	set_project_definitions()
-	set_project_compile_flags()
-	set_project_processor_definitions()
-	set_precompiled_header()
-endfunction(set_project_exe_mode_nobrowser)
-
 function(set_precompiled_header)
-		set(FILENAME "stdafx.h")
-		set(SOURCE "${CMAKE_CURRENT_SOURCE_DIR}/stdafx.cpp")
-        set_target_properties(${PROJECT_NAME} PROPERTIES COMPILE_FLAGS "/Yu\"${FILENAME}\" /DUSE_PRECOMPILED_HEADERS")
-        set_source_files_properties(${SOURCE} PROPERTIES COMPILE_FLAGS "/Yc")
+		add_precompiled_header(${PROJECT_NAME} stdafx.h FORCEINCLUDE)
+		#set(FILENAME "stdafx.h")
+		#set(SOURCE "${CMAKE_CURRENT_SOURCE_DIR}/stdafx.cpp")
+        #set_target_properties(${PROJECT_NAME} PROPERTIES COMPILE_FLAGS "/Yu\"${FILENAME}\" /DUSE_PRECOMPILED_HEADERS")
+        #set_source_files_properties(${SOURCE} PROPERTIES COMPILE_FLAGS "/Yc")
 endfunction(set_precompiled_header)
 
 function(set_source_non_precompiled_header filename)
@@ -111,53 +81,66 @@ function(set_source_non_precompiled_header filename)
 endfunction(set_source_non_precompiled_header)
 
 function(set_project_compile_flags)
-		set_target_properties(${PROJECT_NAME} PROPERTIES LINK_FLAGS_UNICODEDEBUG "/INCREMENTAL /DEBUG:FASTLINK /SUBSYSTEM:WINDOWS /SAFESEH:NO")
-		set_target_properties(${PROJECT_NAME} PROPERTIES LINK_FLAGS_UNICODERELEASE "/INCREMENTAL:NO /DEBUG /SUBSYSTEM:WINDOWS /SAFESEH:NO /MAP")
+		if(MSVC)
+			set_target_properties(${PROJECT_NAME} PROPERTIES LINK_FLAGS_UNICODEDEBUG "/INCREMENTAL /DEBUG:FASTLINK /SUBSYSTEM:WINDOWS /SAFESEH:NO")
+			set_target_properties(${PROJECT_NAME} PROPERTIES LINK_FLAGS_UNICODERELEASE "/INCREMENTAL:NO /DEBUG /SUBSYSTEM:WINDOWS /SAFESEH:NO /MAP")
+		elseif(MINGW)		
+		endif()
 endfunction(set_project_compile_flags)
 
 function(set_project_compile_options_debugrelease)
+	if(MSVC)
+		list(APPEND COMPILEOPT_DEBUG
+			/Od           	# Enable basic run-time checks
+			/MDd		  	# Multithreaded debug runtime
+		)
+		list(APPEND COMPILEOPT_RELEASE
+			/O2           	# Optimize for maximum speed
+			/MD           	# Multithreaded release runtime
+			/Ob1		  	# Inline function expansion
+			/GF			  	# Enable string pooling
+		)
+		target_compile_options(${PROJECT_NAME} PUBLIC $<$<CONFIG:UnicodeDebug>:${COMPILEOPT_DEBUG}>)
+		target_compile_options(${PROJECT_NAME} PUBLIC $<$<CONFIG:UnicodeRelease>:${COMPILEOPT_RELEASE}>)
+	elseif(MINGW)	
 	list(APPEND COMPILEOPT_DEBUG
-		/Od           	# Enable basic run-time checks
-		/MDd		  	# Multithreaded debug runtime
-	)
-
-	list(APPEND COMPILEOPT_RELEASE
-		/O2           	# Optimize for maximum speed
-		/MD           	# Multithreaded release runtime
-		/Ob1		  	# Inline function expansion
-		/GF			  	# Enable string pooling
-	)
-	
-	target_compile_options(${PROJECT_NAME} PUBLIC $<$<CONFIG:UnicodeDebug>:${COMPILEOPT_DEBUG}>)
-	target_compile_options(${PROJECT_NAME} PUBLIC $<$<CONFIG:UnicodeRelease>:${COMPILEOPT_RELEASE}>)
+			-g           	# Produce debugging symbols
+			-mthreads
+			-fmessage-length=0
+			-fexceptions
+			-fident
+			-std=gnu++14
+			-Wno-deprecated-declarations
+		)
+		list(APPEND COMPILEOPT_RELEASE
+		    -O2
+			-mthreads
+			-fmessage-length=0
+			-fexceptions
+			-fident
+			-std=gnu++14
+			-Wno-deprecated-declarations
+		)
+		target_compile_options(${PROJECT_NAME} PUBLIC $<$<CONFIG:UnicodeDebug>:${COMPILEOPT_DEBUG}>)
+		target_compile_options(${PROJECT_NAME} PUBLIC $<$<CONFIG:UnicodeRelease>:${COMPILEOPT_RELEASE}>)
+	endif()
 endfunction(set_project_compile_options_debugrelease)
 
 function(set_project_compile_options)
-	list(APPEND COMPILEOPT
-		/Gy			  	# Enable function level linking
-		/Yu			  	# Use preocompiled headers
-		/FR			  	# Enable browse information
-		/Zm800			# Precompiled header memory allocation limit
-		/GR				# #Enable run type information
-	)
+	if(MSVC)
+		list(APPEND COMPILEOPT
+			/Gy			  	# Enable function level linking
+			/Yu			  	# Use preocompiled headers
+			/FR			  	# Enable browse information
+			/Zm800			# Precompiled header memory allocation limit
+			/GR				# #Enable run type information
+		)		
+		target_compile_options(${PROJECT_NAME} PUBLIC  ${COMPILEOPT})	
+	elseif(MINGW)	
 	
-	target_compile_options(${PROJECT_NAME} PUBLIC  ${COMPILEOPT})	
+	endif()
 	set_project_compile_options_debugrelease()
 endfunction(set_project_compile_options)
-
-#Browser infromation is disabled for some projects because of warnings:
-#warning BK4504: file vector(179) contains too many references; ignoring further references from this source
-#warning BK4503: minor error in .SBR file
-function(set_project_compile_options_nobrowserinfo)
-	list(APPEND COMPILEOPT
-		/Gy			  	# Enable function level linking
-		/Yu			  	# Use preocompiled headers
-		/Zm800			# Precompiled header memory allocation limit
-		/GR				# #Enable run type information
-	)
-	target_compile_options(${PROJECT_NAME} PUBLIC  ${COMPILEOPT})		
-	set_project_compile_options_debugrelease()
-endfunction(set_project_compile_options_nobrowserinfo)
 
 function(set_project_processor_definitions)
 	list (APPEND COMPILER_DEFINES
@@ -178,14 +161,12 @@ function(set_project_processor_definitions)
 endfunction(set_project_processor_definitions)
 
 function(set_project_definitions)
-	#Unicode character set
-	add_definitions(-DUNICODE -D_UNICODE)
+	add_definitions(-DUNICODE -D_UNICODE) #Unicode character set
 
-	#Program database
-	add_definitions(/Zi)
-
-	#Multi process compilation
-	add_definitions(/MP)
+	if(MSVC)
+		add_definitions(/Zi)  #Program database
+		add_definitions(/MP)  #Multi process compilation
+	endif()
 	
 	#64bit define
 	IF( "${CMAKE_SIZEOF_VOID_P}" STREQUAL "8" )
